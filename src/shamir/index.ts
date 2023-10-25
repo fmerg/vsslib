9cc5fd192a726f810cef2a5d62a82d03908d3cc3
fmerg <fmerg@protonmail.com>
2023-10-25 12:18:54 +0300

    (shamir): Implement secret share generation primitives

--- src/shamir/index.ts
+++ src/shamir/index.ts
@@ -1,10 +1,25 @@
 import { Key } from '../key';
-import { Point } from '../elgamal/abstract';
+import { Point, Group } from '../elgamal/abstract';
+import { CryptoSystem } from '../elgamal/core';
+import { mod } from '../utils';
+import { Polynomial } from '../lagrange';
+import { Messages } from './enums';
 
 export type KeyShare = {
   key: Key,
   index: number,
 };
+
+
+export type ShareSetup = {
+  nrShares: number,
+  threshold: number,
+  polynomial: Polynomial
+  key: Key,
+  shares: KeyShare[],
+  commitments: Point[],
+};
+
 
 export type DecryptorShare = {
   decryptor: Point,
@@ -12,3 +27,44 @@ export type DecryptorShare = {
   proof: any,   // TODO
 };
 
+
+export const computeCommitments = async (ctx: any, poly: Polynomial): Promise<Point[]> => {
+  const commitments = new Array(poly.degree + 1);
+  for (const [index, coeff] of poly.coeffs.entries()) {
+    commitments[index] = await ctx.operate(coeff, ctx.generator);
+  }
+  return commitments;
+}
+
+
+export const computeKeyShares = async (ctx: any, poly: Polynomial, nrShares: number): Promise<KeyShare[]> => {
+  const shares = [];
+  for (let index = 1; index <= nrShares; index++) {
+    const key = new Key(ctx, poly.evaluate(index));
+    shares.push({ key, index });
+  }
+  return shares;
+}
+
+
+export const shareSecret = async (ctx: any, nrShares: number, threshold: number): Promise<ShareSetup> => {
+  if (threshold > nrShares) throw new Error(Messages.THRESHOLD_EXCEEDS_NR_SHARES);
+  const polynomial = await Polynomial.random({ degree: threshold - 1, order: ctx.order });
+  const key = new Key(ctx, polynomial.coeffs[0]);
+  const shares = await computeKeyShares(ctx, polynomial, nrShares);
+  const commitments = await computeCommitments(ctx, polynomial);
+  return { nrShares, threshold, polynomial, key, shares, commitments };
+}
+
+
+export const verifyKeyShare = async (ctx: any, share: KeyShare, commitments: Point[]): Promise<boolean> => {
+  const { key, index: i } = share;
+  const target = await ctx.operate(key.secret, ctx.generator);
+  let acc = ctx.neutral;
+  for (const [j, comm] of commitments.entries()) {
+    const curr = await ctx.operate(mod(BigInt(i ** j), ctx.order), comm);
+    acc = await ctx.combine(acc, curr);
+  }
+  if (!(await acc.isEqual(target))) throw new Error(Messages.INVALID_KEY_SHARE);
+  return true;
+}
--- tests/shamir.spec.ts
+++ tests/shamir.spec.ts
@@ -11,49 +11,27 @@ import { partialPermutations } from './helpers';
 describe('demo', () => {
   test('demo 1 - sharing with dealer', async () => {
     const label = 'ed25519';
-
-    // Setup from (t, n)
-    // TODO: Assert t <= n
+    const ctx = elgamal.initCrypto(label);
     const n = 5;
     const t = 3;
-    const ctx = elgamal.initCrypto(label);
-    const { order } = ctx;
-    const degree = t - 1;
-    const poly = await Polynomial.random({ degree, order });
-    const key = new Key(ctx, poly.coeffs[0]);
-    // Compute commitments
-    const commitments: Point[] = [];
-    for (let i = 0; i < t; i++) {
-      const comm = await ctx.operate(poly.coeffs[i], ctx.generator);
-      commitments.push(comm);
-    }
-    // Compute key shares
-    const shares: KeyShare[] = [];
-    for (let i = 1; i <= n; i++) {
-      const key = new Key(ctx, poly.evaluate(i));
-      const index = i;
-      shares.push({
-        key,
-        index,
-      });
-    }
-    const setup = { n, t, key, poly, shares, commitments };
-    expect(setup.n).toEqual(n);
-    expect(setup.t).toEqual(t);
-    expect(setup.shares.length).toEqual(n);
-    expect(setup.poly.degree).toEqual(t - 1);
+    const {
+      nrShares,
+      threshold,
+      polynomial,
+      key,
+      shares,
+      commitments,
+    } = await shamir.shareSecret(ctx, n, t);
+    expect(nrShares).toEqual(n);
+    expect(threshold).toEqual(t);
+    expect(shares.length).toEqual(n);
+    expect(polynomial.degree).toEqual(t - 1);
     expect(commitments.length).toEqual(t);
 
-    // Verify computation of each private share
-    shares.forEach(async (share) => {
-      const { key, index: i } = share;
-      const target = await ctx.operate(key.secret, ctx.generator);
-      let acc = ctx.neutral;
-      for (let j = 0; j < commitments.length; j++) {
-        const curr = await ctx.operate(mod(BigInt(i ** j), ctx.order), commitments[j]);
-        acc = await ctx.combine(acc, curr);
-      }
-      expect(await acc.isEqual(target)).toBe(true);
+    // Verify computation of each key share
+    shares.forEach(async (share: KeyShare ) => {
+      const isValid = await shamir.verifyKeyShare(ctx, share, commitments);
+      expect(isValid).toBe(true);
     });
 
     // Reconstruct key for each combination of involved parties
@@ -83,33 +61,22 @@ describe('demo', () => {
   });
   test('demo 3 - threshold decryption', async () => {
     const label = 'ed25519';
-
-    // Setup from (t, n)
-    // TODO: Assert t <= n
+    const ctx = elgamal.initCrypto(label);
     const n = 3;
     const t = 2;
-    const ctx = elgamal.initCrypto(label);
-    const { order } = ctx;
-    const degree = t - 1;
-    const poly = await Polynomial.random({ degree, order });
-    const key = new Key(ctx, poly.coeffs[0]);
-    // Compute commitments
-    const commitments: Point[] = [];
-    for (let i = 0; i < t; i++) {
-      const comm = await ctx.operate(poly.coeffs[i], ctx.generator);
-      commitments.push(comm);
-    }
-    // Compute key shares
-    const shares: KeyShare[] = [];
-    for (let i = 1; i <= n; i++) {
-      const key = new Key(ctx, poly.evaluate(i));
-      const index = i;
-      shares.push({
-        key,
-        index,
-      });
-    }
-    const setup = { n, t, key, poly, shares, commitments };
+    const {
+      nrShares,
+      threshold,
+      polynomial,
+      key,
+      shares,
+      commitments,
+    } = await shamir.shareSecret(ctx, n, t);
+    expect(nrShares).toEqual(n);
+    expect(threshold).toEqual(t);
+    expect(shares.length).toEqual(n);
+    expect(polynomial.degree).toEqual(t - 1);
+    expect(commitments.length).toEqual(t);
 
     // Encrypt something with respect to the combined public key
     const message = await ctx.randomPoint();
@@ -139,6 +106,7 @@ describe('demo', () => {
         const isValid = await ctx.verifyDecryptor(dshare, ciphertext, pubi, proof);
         expect(isValid).toBe(true);
         // Compute lambdai
+        const { order } = ctx;
         let lambdai = BigInt(1);
         qualifiedIndexes.forEach(j => {
           if (i != j) {
