365b76b2c0707f4e90a9bec380490de419aa9f32
fmerg <fmerg@protonmail.com>
2024-05-23 13:40:14 +0300

    (tests): Refactor keys tests

---
+++ tests/keys/decryptor.spec.ts
@@ -0,0 +1,115 @@
+import { Algorithms } from '../../src/enums';
+import { generateKey } from '../../src';
+import { ErrorMessages } from '../../src/errors';
+import { randomNonce } from '../../src/crypto';
+import { cartesian } from '../helpers';
+import { mockMessage } from './helpers';
+import { resolveTestConfig } from '../environ';
+
+const { systems, elgamalSchemes: schemes, aesModes, algorithms } = resolveTestConfig();
+
+
+describe('generation', () => {
+  it.each(cartesian([systems, schemes]))('over %s%/s', async (system, scheme) => {
+    const { privateKey, publicKey, ctx } = await generateKey(system);
+    const message = await mockMessage(ctx, scheme);
+    const { ciphertext, decryptor: targetDecryptor } = await publicKey.encrypt(message, { scheme });
+    const { decryptor, proof } = await privateKey.generateDecryptor(ciphertext);
+    expect(decryptor).toEqual(targetDecryptor);
+    expect(await publicKey.verifyDecryptor(ciphertext, decryptor, proof)).toBe(true);
+  });
+});
+
+
+describe('proof - success without nonce', () => {
+  it.each(cartesian([systems, schemes, algorithms]))('over %s/%s', async (
+    system, scheme, algorithm
+  ) => {
+    const { privateKey, publicKey, ctx } = await generateKey(system);
+    const message = await mockMessage(ctx, scheme);
+    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
+    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { algorithm });
+    const verified = await publicKey.verifyDecryptor(ciphertext, decryptor, proof, { algorithm });
+    expect(verified).toBe(true);
+  });
+});
+
+
+describe('proof - success with nonce', () => {
+  it.each(cartesian([systems, schemes, algorithms]))('over %s/%s', async (
+    system, scheme, algorithm
+  ) => {
+    const { privateKey, publicKey, ctx } = await generateKey(system);
+    const message = await mockMessage(ctx, scheme);
+    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
+    const nonce = await randomNonce();
+    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { algorithm, nonce });
+    const verified = await publicKey.verifyDecryptor(ciphertext, decryptor, proof, { algorithm, nonce });
+    expect(verified).toBe(true);
+  });
+});
+
+
+describe('proof - failure if forged proof', () => {
+  it.each(cartesian([systems, schemes]))('over %s', async (system, scheme) => {
+    const { privateKey, publicKey, ctx } = await generateKey(system);
+    const message = await mockMessage(ctx, scheme);
+    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
+    const proof = await privateKey.proveDecryptor(ciphertext, decryptor);
+    proof.commitment[0] = (await ctx.randomPoint()).toBytes();
+    await expect(publicKey.verifyDecryptor(ciphertext, decryptor, proof)).rejects.toThrow(
+      ErrorMessages.INVALID_DECRYPTOR
+    );
+  });
+});
+
+
+describe('proof - failure if wrong algorithm', () => {
+  it.each(cartesian([systems, schemes, algorithms]))('over %s/%s/%s', async (
+    system, scheme, algorithm
+  ) => {
+    const { privateKey, publicKey, ctx } = await generateKey(system);
+    const message = await mockMessage(ctx, scheme);
+    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
+    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { algorithm });
+    await expect(
+      publicKey.verifyDecryptor(ciphertext, decryptor, proof, {
+        algorithm: algorithm == Algorithms.SHA256 ?
+          Algorithms.SHA512 :
+          Algorithms.SHA256
+      })
+    ).rejects.toThrow(
+      ErrorMessages.INVALID_DECRYPTOR
+    );
+  });
+});
+
+
+describe('proof - failure if missing nonce', () => {
+  it.each(cartesian([systems, schemes]))('over %s/%s', async (system, scheme) => {
+    const { privateKey, publicKey, ctx } = await generateKey(system);
+    const message = await mockMessage(ctx, scheme);
+    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
+    const nonce = await randomNonce();
+    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { nonce });
+    await expect(publicKey.verifyDecryptor(ciphertext, decryptor, proof)).rejects.toThrow(
+      ErrorMessages.INVALID_DECRYPTOR
+    );
+  });
+});
+
+
+describe('proof - failure if forged nonce', () => {
+  it.each(cartesian([systems, schemes]))('over %s/%s', async (system, scheme) => {
+    const { privateKey, publicKey, ctx } = await generateKey(system);
+    const message = await mockMessage(ctx, scheme);
+    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
+    const nonce = await randomNonce();
+    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { nonce });
+    await expect(
+      publicKey.verifyDecryptor(ciphertext, decryptor, proof, { nonce: await randomNonce() })
+    ).rejects.toThrow(
+      ErrorMessages.INVALID_DECRYPTOR
+    );
+  });
+});
--- tests/keys/encryption.spec.ts
+++ tests/keys/encryption.spec.ts
@@ -1,21 +1,12 @@
-import { Algorithms, AesModes, ElgamalSchemes } from '../../src/enums';
-import { Algorithm, ElgamalScheme } from '../../src/types';
+import { Algorithms, ElgamalSchemes } from '../../src/enums';
 import { generateKey } from '../../src';
-import { PrivateKey, PublicKey } from '../../src/keys';
 import { ErrorMessages } from '../../src/errors';
 import { randomNonce } from '../../src/crypto';
 import { cartesian } from '../helpers';
+import { mockMessage } from './helpers';
 import { resolveTestConfig } from '../environ';
-import { Point, Group } from '../../src/backend/abstract';
 
 const { systems, elgamalSchemes: schemes, aesModes, algorithms } = resolveTestConfig();
-
-
-const mockMessage = async (
-  ctx: Group<Point>, scheme: ElgamalScheme
-) => scheme == ElgamalSchemes.PLAIN ? (await ctx.randomPoint()).toBytes() :
-  Uint8Array.from(Buffer.from('destroy earth'));
-
 
 
 describe('encrypt and decrypt', () => {
@@ -138,112 +129,6 @@ describe('encrypt-then-prove - failure if forged nonce', () => {
       privateKey.verifyEncryption(ciphertext, proof, { nonce: await randomNonce() })
     ).rejects.toThrow(
       ErrorMessages.INVALID_ENCRYPTION
-    );
-  });
-});
-
-
-describe('Decryptor generation', () => {
-  it.each(cartesian([systems, schemes]))('over %s%/s', async (system, scheme) => {
-    const { privateKey, publicKey, ctx } = await generateKey(system);
-    const message = await mockMessage(ctx, scheme);
-    const { ciphertext, decryptor: targetDecryptor } = await publicKey.encrypt(message, { scheme });
-    const { decryptor, proof } = await privateKey.generateDecryptor(ciphertext);
-    expect(decryptor).toEqual(targetDecryptor);
-    expect(await publicKey.verifyDecryptor(ciphertext, decryptor, proof)).toBe(true);
-  });
-});
-
-
-describe('Decryptor proof - success without nonce', () => {
-  it.each(cartesian([systems, schemes, algorithms]))('over %s/%s', async (
-    system, scheme, algorithm
-  ) => {
-    const { privateKey, publicKey, ctx } = await generateKey(system);
-    const message = await mockMessage(ctx, scheme);
-    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
-    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { algorithm });
-    const verified = await publicKey.verifyDecryptor(ciphertext, decryptor, proof, { algorithm });
-    expect(verified).toBe(true);
-  });
-});
-
-
-describe('Decryptor proof - success with nonce', () => {
-  it.each(cartesian([systems, schemes, algorithms]))('over %s/%s', async (
-    system, scheme, algorithm
-  ) => {
-    const { privateKey, publicKey, ctx } = await generateKey(system);
-    const message = await mockMessage(ctx, scheme);
-    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
-    const nonce = await randomNonce();
-    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { algorithm, nonce });
-    const verified = await publicKey.verifyDecryptor(ciphertext, decryptor, proof, { algorithm, nonce });
-    expect(verified).toBe(true);
-  });
-});
-
-
-describe('Decryptor proof - failure if forged proof', () => {
-  it.each(cartesian([systems, schemes]))('over %s', async (system, scheme) => {
-    const { privateKey, publicKey, ctx } = await generateKey(system);
-    const message = await mockMessage(ctx, scheme);
-    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
-    const proof = await privateKey.proveDecryptor(ciphertext, decryptor);
-    proof.commitment[0] = (await ctx.randomPoint()).toBytes();
-    await expect(publicKey.verifyDecryptor(ciphertext, decryptor, proof)).rejects.toThrow(
-      ErrorMessages.INVALID_DECRYPTOR
-    );
-  });
-});
-
-
-describe('Decryptor proof - failure if wrong algorithm', () => {
-  it.each(cartesian([systems, schemes, algorithms]))('over %s/%s/%s', async (
-    system, scheme, algorithm
-  ) => {
-    const { privateKey, publicKey, ctx } = await generateKey(system);
-    const message = await mockMessage(ctx, scheme);
-    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
-    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { algorithm });
-    await expect(
-      publicKey.verifyDecryptor(ciphertext, decryptor, proof, {
-        algorithm: algorithm == Algorithms.SHA256 ?
-          Algorithms.SHA512 :
-          Algorithms.SHA256
-      })
-    ).rejects.toThrow(
-      ErrorMessages.INVALID_DECRYPTOR
-    );
-  });
-});
-
-
-describe('Decryptor proof - failure if missing nonce', () => {
-  it.each(cartesian([systems, schemes]))('over %s/%s', async (system, scheme) => {
-    const { privateKey, publicKey, ctx } = await generateKey(system);
-    const message = await mockMessage(ctx, scheme);
-    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
-    const nonce = await randomNonce();
-    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { nonce });
-    await expect(publicKey.verifyDecryptor(ciphertext, decryptor, proof)).rejects.toThrow(
-      ErrorMessages.INVALID_DECRYPTOR
-    );
-  });
-});
-
-
-describe('Decryptor proof - failure if forged nonce', () => {
-  it.each(cartesian([systems, schemes]))('over %s/%s', async (system, scheme) => {
-    const { privateKey, publicKey, ctx } = await generateKey(system);
-    const message = await mockMessage(ctx, scheme);
-    const { ciphertext, decryptor } = await publicKey.encrypt(message, { scheme });
-    const nonce = await randomNonce();
-    const proof = await privateKey.proveDecryptor(ciphertext, decryptor, { nonce });
-    await expect(
-      publicKey.verifyDecryptor(ciphertext, decryptor, proof, { nonce: await randomNonce() })
-    ).rejects.toThrow(
-      ErrorMessages.INVALID_DECRYPTOR
     );
   });
 });
